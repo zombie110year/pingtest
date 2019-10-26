9d89904e034bf8f5cf81d2f26d48cdadb3b90493
zombie110year <zombie110year@outlook.com>
2019-10-26 17:08:23 +0800

    单元测试中打错字母

--- tests/test_icmp.py
+++ tests/test_icmp.py
@@ -1,7 +1,7 @@
 from pingtest.icmp import ICMPMessage
 
 def test_icmp_checksum():
-    assert ICMPMessage(b"abcdefghijklmnoqrstuvwxyz123456").checksum == 0xc83f
+    assert ICMPMessage(b"abcdefghijklmnopqrstuvwxyz123456").checksum == 0xcfc7
     assert ICMPMessage(b"").checksum == 0xf7ff
     assert ICMPMessage(b"\x00\x00\x00\x00").checksum == 0xf7ff
     assert ICMPMessage(b"\x00\x01\x02").checksum == 0xf5fe
