44e3099ea4fe21508c92e59690cc81facc1833f5
zombie110year <zombie110year@outlook.com>
2019-10-26 16:54:14 +0800

    :bug: ICMP 检验和算法错误

--- tests/test_icmp.py
+++ tests/test_icmp.py
@@ -1,23 +1,7 @@
 from pingtest.icmp import ICMPMessage
-from pingtest.icmp import icmp_checksum
 
 def test_icmp_checksum():
-    assert icmp_checksum(b"\x08\x00" # type, code
-                        b"\x00\x00" # checksum = 0
-                        b"\x01\x00" # id
-                        b"\x00\x00" # seq
-                        # content
-                        b"\x00\x00\x11\x22") == 0x1a22
-    assert icmp_checksum(b"\x08\x00" # type, code
-                        b"\x00\x00" # checksum = 0
-                        b"\x01\x00" # id
-                        b"\x00\x00" # seq
-                        # content
-                        b"\x00\x00\x11") == 0x1a00
-
-
-def test_ICMPMessage_pack():
-    x = ICMPMessage(b"Hello World", ICMPMessage.TYPE_ECHO_REQUEST)
-    assert x.pack() == b"\x08\x00\x59\xcc\x00\x00\x00\x00Hello World"
-    assert x.pack() == b"\x08\x00\x59\xcc\x00\x00\x00\x00Hello World"
-    assert x.pack() == b"\x08\x00\x59\xcc\x00\x00\x00\x00Hello World"
+    assert ICMPMessage(b"abcdefghijklmnoqrstuvwxyz123456").checksum == 0xc83f
+    assert ICMPMessage(b"").checksum == 0xf7ff
+    assert ICMPMessage(b"\x00\x00\x00\x00").checksum == 0xf7ff
+    assert ICMPMessage(b"\x00\x01\x02").checksum == 0xf5fe
