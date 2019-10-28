4cad71c0bba9baae840c4c697fd22f153951c362
zombie110year <zombie110year@outlook.com>
2019-10-28 22:12:11 +0800

    :sparkles: 在根模块添加公共入口

--- pingtest/__init__.py
+++ pingtest/__init__.py
@@ -1,1 +1,8 @@
+from .icmp import ICMPConnect
+from .icmp import ICMPMessage
+from .ping import GNUPing
+from .ping import MicrosoftPing
+from .ping import Ping
+from .ping import PingReport
+
 __version__ = '0.1.0'
